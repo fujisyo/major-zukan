7128fa9a045a2c3851cff0cf678cd35a4361e606
fujisyo <kinggggsyot@gmail.com>
2020-01-26 17:42:25 +0900

    setting

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   # belongs_to :nasyonal
   # belongs_to :amerikan
   mount_uploader :image, ImagesUploader
-  validates :name, presence: true
+  validates :name,:image, presence: true
 end
