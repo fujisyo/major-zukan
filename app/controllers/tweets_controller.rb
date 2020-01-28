3f01b229509fcaa3ce6617c6588113d779ab7ec3
fujisyo <kinggggsyot@gmail.com>
2020-01-28 17:35:58 +0900

    setting_delete

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,11 +8,24 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.create(tweet_params) 
     if @tweet.save
-      redirect_to 'tweets_path'
+      redirect_to "tweets_path"
     else
       render 'new'
     end
   end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.destroy
+      redirect_to 'tweets_path'
+    end
+  end
+
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
