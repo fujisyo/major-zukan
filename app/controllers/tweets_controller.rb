23eae26177a9a147a6c687f8266d2c8bde1f9a80
fujisyo <kinggggsyot@gmail.com>
2020-01-28 14:51:27 +0900

    setting_index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,13 @@
 class TweetsController < ApplicationController
   def index
   end
+  def new
+    @tweet = Tweet.new
+  end
+  # def create
+  #   @tweet = Tweet.create(tweet_params) 
+  # end
+  # private
+  # def tweet_params
+
 end
