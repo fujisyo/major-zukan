9481a53fcf4cb82a2ad67df96c90436c06c402e6
fujisyo <kinggggsyot@gmail.com>
2020-01-29 13:02:09 +0900

    setting_migrate

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20200128043450) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
