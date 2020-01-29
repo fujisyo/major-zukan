2577cc346d657d79355587471b122b2a16393893
fujisyo <kinggggsyot@gmail.com>
2020-01-29 19:17:28 +0900

    setting_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+    redirect_to tweet_comments_path(comment.tweet.id) # コメントと結びつくツイートの詳細画面に遷移する
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
