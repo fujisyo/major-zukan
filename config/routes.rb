5748231207c63017da7d57ed6264807262b4ddc8
fujisyo <kinggggsyot@gmail.com>
2020-01-31 19:01:24 +0900

    setting_api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets do
     resources :comments, only: :create
+    namespace :api do
+      resources :comments, only: :index, defaults: { format: 'json' }
+    end
   end
   resources :teams , only: [:index, :show]
   resources :players, only: [:index, :show, :new, :create] do
