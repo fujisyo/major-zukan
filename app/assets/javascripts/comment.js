cbf67125cd48fbfc887c937748324f6aac3d1743
fujisyo <kinggggsyot@gmail.com>
2020-02-01 15:16:44 +0900

    setting

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,7 +1,7 @@
 $(function(){
   function buildHTML(comment){
-    var html = `<div class="umain__comment__title__content__name">
-                  <a class="umain__comment__title__content__name__user" href=/users/${comment.user_id}>${comment.user_name}</a>
+    var html = `<div class="umain__comment__title__content__name" data-comment-id="${comment.id}">
+                <a class="umain__comment__title__content__name__user" href=/users/${comment.user_id}>${comment.user_name}</a>
                   <div class="umain__content__title__content__name__text">
                     ${comment.text}
                     <div class="umain__comment__title__content__name__text__time">
@@ -31,6 +31,32 @@ $(function(){
     })
     .fail(function(){
       alert('error');
-    })
-  })
+    });
+  });
+  var reloadMessages = function() { 
+    if(window.location.href.match(/\/tweets\/\d+\/comments/)){
+      last_comment_id = $(".umain__comment__title__content__name").last().data("message-id")
+      $.ajax({
+        url: 'api/comments',
+        type: 'GET',
+        dataType: 'json',
+        data: {id: last_comment_id}
+      })
+      .done(function(comments) {
+        var insertHTML = '';
+        $.each(comments, function(i, comment){
+          insertHTML += buildHTML(comment)
+        });
+        // if (comments.length != 0){
+        $('.umain__comment__title__content__name').append(insertHTML);
+        $('.umain__comment__title__content__name').animate({scrollBootom: $('.umain__comment__title__content__name')[0].scrollHeight});
+        // }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
+
+}
+  setInterval(reloadMessages, 7000);
 })
---
+++ app/assets/javascripts/update.js
@@ -0,0 +1,5 @@
+// $(function(){
+//   last_message_id = $('.umain__comment__title__content__name:last').data("comment-id");
+//   console.log(last_comment_id);
+
+// })
