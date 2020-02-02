f267b5d6984ee09e236885e2234cae8e34de5987
fujisyo <kinggggsyot@gmail.com>
2020-02-02 15:17:28 +0900

    setting_scroll

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -26,5 +26,16 @@ $(function(){
       }
     });
   });
+  $(window).scroll(function(){
+    $('.pmain__sidebar').each(function(){
+      var targetElement = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > targetElement - windowHeight + 600){
+        $(this).css('opacity','1');
+        $(this).css('transform','translateY(0)');
+      }
+    });
+  });
 
 });
