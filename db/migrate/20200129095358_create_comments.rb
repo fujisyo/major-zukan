62abecee2fc3e6ee78758bb8f25e19e96a96cca8
fujisyo <kinggggsyot@gmail.com>
2020-02-03 13:54:45 +0900

    setting

--- db/migrate/20200129095358_create_comments.rb
+++ db/migrate/20200129095358_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :tweet, index: true, foreign_key: true,
+      t.references :tweet, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
       t.timestamps
     end
